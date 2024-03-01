be544c1937b26698028c66e0e622b6a74039ba22
hamzabdel <hamzaabdel@csu.fullerton.edu>
2024-02-29 17:01:22 -0800

    updated landing pages

--- ddos-project/src/routes/+page.svelte
+++ ddos-project/src/routes/+page.svelte
@@ -1,22 +1,24 @@
-<script> import { P, Layout } from 'flowbite-svelte';</script>
+<script> 
+  import { P, Layout } from 'flowbite-svelte';
+
+  function handleClick() {
+    window.location.href = "/about";
+    window.location.href = "/map";
+    window.location.href = "/graphs";
+  }
+</script>
+
 <style>
     :global(body) {
       @apply bg-neutral-50;
     }
   </style>
 
-<body class = m-0>
-<<<<<<< HEAD
 <nav class="flex items-center justify-center flex-wrap bg-sky-600 p-6 ">
-=======
-<nav class="flex items-center justify-between flex-wrap bg-zinc-600 p-6 ">
->>>>>>> 7e82baf107fe47596970b54a095cc5f1a94e2bb6
     <div class="flex items-center flex-shrink-0 text-white mr-4">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
             <path d="M16.555 5.412a8.028 8.028 0 0 0-3.503-2.81 14.899 14.899 0  0 1 1.663 4.472 8.547 8.547 0 0 0 1.84-1.662ZM13.326 7.825a13.43 13.43 0 0 0-2.413-5.773 8.087 8.087 0 0 0-1.826 0 13.43 13.43 0 0 0-2.413 5.773A8.473 8.473 0 0 0 10 8.5c1.18 0 2.304-.24 3.326-.675ZM6.514 9.376A9.98 9.98 0 0 0 10 10c1.226 0 2.4-.22 3.486-.624a13.54 13.54 0 0 1-.351 3.759A13.54 13.54 0 0 1 10 13.5c-1.079 0-2.128-.127-3.134-.366a13.538 13.538 0 0 1-.352-3.758ZM5.285 7.074a14.9 14.9 0 0 1 1.663-4.471 8.028 8.028 0 0 0-3.503 2.81c.529.638 1.149 1.199 1.84 1.66ZM17.334 6.798a7.973 7.973 0 0 1 .614 4.115 13.47 13.47 0 0 1-3.178 1.72 15.093 15.093 0 0 0 .174-3.939 10.043 10.043 0 0 0 2.39-1.896ZM2.666 6.798a10.042 10.042 0 0 0 2.39 1.896 15.196 15.196 0 0 0 .174 3.94 13.472 13.472 0 0 1-3.178-1.72 7.973 7.973 0 0 1 .615-4.115ZM10 15c.898 0 1.778-.079 2.633-.23a13.473 13.473 0 0 1-1.72 3.178 8.099 8.099 0 0 1-1.826 0 13.47 13.47 0 0 1-1.72-3.178c.855.151 1.735.23 2.633.23ZM14.357 14.357a14.912 14.912 0 0 1-1.305 3.04 8.027 8.027 0 0 0 4.345-4.345c-.953.542-1.971.981-3.04 1.305ZM6.948 17.397a8.027 8.027 0 0 1-4.345-4.345c.953.542 1.971.981 3.04 1.305a14.912 14.912 0 0 0 1.305 3.04Z" />
           </svg>
-<<<<<<< HEAD
-          
       <span class="font-semibold text-xl tracking-tight">NetSafely</span>
     </div>
     <div class="block lg:hidden">
@@ -36,24 +38,11 @@
           Graphs
         </a>
         <a href="/srclink" class="block mt-4 lg:inline-block lg:mt-0 text-sky-50 hover:text-white">
-          Github Link
+          Source
         </a>
       </div>
     </div>
   </nav>
 
-</body>
 
 
-=======
-      <span class="font-semibold text-indigo-300 text-xl tracking-tight">NetGuard</span>
-<ul class = "list-none text-center inline"> 
-  <a href="/dashboard" class="font-bold px-3 py-2 text-indigo-300 rounded-lg hover:bg-slate-100 hover:text-slate-900 no-underline">About</a>
-  <a href="/team" class="font-bold px-3 py-2 text-indigo-300 rounded-lg hover:bg-slate-100 hover:text-slate-900 no-underline">Map</a>
-  <a href="/projects" class="font-bold px-3 py-2 text-indigo-300 rounded-lg hover:bg-slate-100 hover:text-slate-900 no-underline">Data</a>
-  <a href="/reports" class="font-bold px-3 py-2 text-indigo-300 rounded-lg hover:bg-slate-100 hover:text-slate-900 no-underline">Source Code</a>
-  </ul>
-  </nav>
-
-</body>
->>>>>>> 7e82baf107fe47596970b54a095cc5f1a94e2bb6
