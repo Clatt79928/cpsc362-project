f9596cc125e59827def96d38f327f3219d53ebb7
hamzabdel <hamzaabdel@csu.fullerton.edu>
2024-03-26 16:17:11 -0700

    edit landing page

--- ddos-project/src/lib/DefaultPage.svelte
+++ ddos-project/src/lib/DefaultPage.svelte
@@ -1,12 +1,10 @@
-
-<div class= "h-96 flex items-center justify-center"> 
-    <h3 class = "font-mono text-4xl animate-rotate-y animate-ease-linear animate-once ">
-                What's the purpose of this project?
+<div class="h-48 flex items-center justify-center"> 
+    <h3 class="font-mono text-4xl animate-rotate-y animate-ease-linear animate-once pt-1">
+        What's is our objective?
     </h3>
 </div>
 
-<h5 class = "h-24 flex items-center justify-center font-mono" >
+<h5 class="h-24 flex items-center justify-center font-mono">
     Inform users on practicing safe browsing and cybersecurity to for protecting against threats, ensuring privacy, 
     and maintaining the integrity of digital experiences!
- </h5>
-
+</h5>
